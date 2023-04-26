c965b5bf7af5e20902eabab54fd14f758077f44d
Gabriel L. Manor <gemanor@Gabriels-MacBook-Pro.local>
2023-04-26 15:10:10 +0300

    :rocket: initial version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,27 +6,27 @@ import {Box} from "@mui/material";
 import {info} from "../info/Info";
 
 const links = [
-    {
-        name: 'Home',
-        to: '/',
-        active: 'home'
-    },
-    {
-        name: 'About Me',
-        to: '/about',
-        active: 'about'
-    },
-    {
-        name: info.initials,
-        type: 'initials',
-        to: '/',
-        active: 'home'
-    },
-    {
-        name: 'Portfolio',
-        to: '/portfolio',
-        active: 'portfolio'
-    }
+    // {
+    //     name: 'Home',
+    //     to: '/',
+    //     active: 'home'
+    // },
+    // {
+    //     name: 'About Me',
+    //     to: '/about',
+    //     active: 'about'
+    // },
+    // {
+    //     name: info.initials,
+    //     type: 'initials',
+    //     to: '/',
+    //     active: 'home'
+    // },
+    // {
+    //     name: 'Portfolio',
+    //     to: '/portfolio',
+    //     active: 'portfolio'
+    // }
 ]
 
 export default function Navbar({darkMode, handleClick}) {
